0c3e8e4205a2fc0fe03735e689cc5c6a431b5f4c
Nico <ncgreco1440@gmail.com>
2018-01-24 08:52:47 -0500

    module adding files to logger'

--- src/index.js
+++ src/index.js
@@ -12,18 +12,56 @@ const validate = {
 	}
 };
 
-const winstonParseConfig = function(arg) {
+const setTransportHandle = function(transport) {
+	switch(transport.level) {
+		case 'info': {
+			this.infoTransport = new winston.transports.File({
+				filename: appendDate(transport.filepath), 
+				level: transport.level
+			});
+			break;
+		}
+		case 'error': {
+			this.errorTransport = new winston.transports.File({
+				filename: appendDate(transport.filepath), 
+				level: transport.level
+			});
+			break;
+		}
+		case 'warn': {}
+		case 'warning': {
+			this.warningTransport =	new winston.transports.File({
+				filename: appendDate(transport.filepath), 
+				level: transport.level
+			});
+			break;
+		}
+		default: {}
+	}
+};
+
+const addTransports = function() {
+	if(this.infoTransport) this.winstonHandle.add(this.infoTransport);
+	if(this.errorTransport) this.winstonHandle.add(this.errorTransport);
+	if(this.warningTransport) this.winstonHandle.add(this.warningTransport);
+};
+
+const updateTransports = function() {
+	debugger;
+	var info = this.infoTransport.filename,	
+		error = this.errorTransport.filename,
+		warning = this.warningTransport.filename;
+};
+
+const winstonParseConfig = function(config) {
 	var obj = {
-		level: arg.leve,
-		format: (arg.format == 'json' || arg.format == 'JSON') ? winston.format.json() : null,
+		level: config.level,
+		format: (config.format == 'json' || config.format == 'JSON') ? winston.format.json() : null,
 		transports: []
 	};
 
-	arg.transports.forEach((transport) => {
-		obj.transports.push(new winston.transports.File({
-			filename: appendDate(transport.filepath), 
-			level: transport.level
-		}));
+	config.transports.forEach((transport) => {
+		(setTransportHandle.bind(this, transport))();
 	});
 
 	return obj;
@@ -45,23 +83,25 @@ const appendDate = function(filename) {
 };
 
 var logger = function(config) {
+	var runConfiguration = winstonParseConfig.bind(this, config),
+		addLoggingTransports = addTransports.bind(this);
+
 	if(!validate.typeOfObject(config))
 		throw new Error('\'config\' was not an object.');
 	if(!validate.containsRequiredProperties(config))
-		throw new Error('\'config\' was missing one or many of the mandatory properties. \'level\', \'format\', \'transports\'');
+		throw new Error('\'config\' was missing one or more of the mandatory properties. \'level\', \'format\', \'transports\'');
 
 	this.winstonHandle = null;
-	this.dailyTimer = null;
-	this.currentTime = null;
-	// Time, in milliseconds, until Midnight
-	this.timeTilMidnight = null;
 	this.dailyThreshold = null;
-	// Time, in milliseconds, within 24 hours;
 	this.twentyFourHourExt = 1000 * 60 * 60 * 24;
+	this.infoTransport = null;
+	this.errorTransport = null;
+	this.warningTransport = null;
+	this.updateTransports = updateTransports.bind(this);
 
 	try {
-		this.winstonHandle = winston.createLogger(winstonParseConfig(config));
-		this.currentTime = Date.now();
+		this.winstonHandle = winston.createLogger(runConfiguration());
+		addLoggingTransports();
 		this.dailyThreshold = clock.eod();
 	}catch(e) {
 		throw e;
@@ -75,7 +115,7 @@ logger.prototype.handle = function() {
 logger.prototype.log = function(lvl, req, status, details) {
 	if(Date.now() > this.dailyThreshold) {
 		this.dailyThreshold += this.twentyFourHourExt;
-		//TODO...update the transports.
+		this.updateTransports();
 	}
 	return this.winstonHandle.log({
 		'level': lvl,
